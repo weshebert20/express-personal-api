c47368e7eeff8d34e6ce3b9e4c9629d42a507d0d
Zeb Tim Girouard <ZebGirouard@users.noreply.github.com>
2016-07-11 11:35:32 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || 
                   process.env.MONGOLAB_URI || 
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/personal-api”);
+                  "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
