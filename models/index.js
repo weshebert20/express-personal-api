afaa3716846673a7bb5e35c940c645fdab6aa52d
Zeb Tim Girouard <ZebGirouard@users.noreply.github.com>
2016-07-11 11:35:15 -0600

    Adding new URI cuz mLab was cool and changed their API variables

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
-                  "mongodb://localhost/personal-api");
+mongoose.connect( process.env.MONGODB_URI || 
+                  process.env.MONGOLAB_URI || 
+                  process.env.MONGOHQ_URL || 
+                  "mongodb://localhost/personal-api”);
 
 // module.exports.Campsite = require("./campsite.js.example");
